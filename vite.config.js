0e1feba5aff7539e9120b2cda98168e60bf2cddf
Romana Gruber <romana.gruber@gmx.net>
2025-04-17 20:30:36 +0200

    Update base in config file

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'pcee',
+  base: '/pcee/',
   plugins: [
     vue(),
     vueDevTools(),
