98f09f9097f6f5a0b354d03a416dada308b72fec
Romana Gruber <romana.gruber@gmx.net>
2025-04-17 21:36:53 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import plugin404 from 'vite-plugin-404'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
